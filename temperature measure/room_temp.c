9432ec4bdcc06c362621a3ec952d917043a890d8
tiansy97 <59889947+tiansy97@users.noreply.github.com>
2020-07-18 18:47:16 -0400

    Update room_temp.c

--- temperature measure/room_temp.c
+++ temperature measure/room_temp.c
@@ -3,7 +3,7 @@ Author:     Siyuan Tian (tiansy97), Ruiqi Hu
 Time:       2018/11
 Functions:  The system measures the room temperature using a temperature sensor. The analog
             output is converted to diginal signals by the ADC0809 chip. The code reads the 
-            signal and converted to real temperatures, and it applied a digital filter to 
+            signal and converts it to real temperatures, and it applies a digital filter to 
             stablize the readings. It then displays the temperature on LEDs.    
 Concepts:   Sensor circuit design, A/D conversion, digital filtering, segment displays, etc  
 */
